3377ec16c38246bee3cbc166458e51755db0cf7e
Ilya Chistousov <kw4s1k2@gmail.com>
2022-03-25 23:01:31 +0500

    Initial commit

--- src/main/java/mapper/FromEventToEventDtoMapper.java
+++ src/main/java/mapper/FromEventToEventDtoMapper.java
@@ -3,16 +3,13 @@ package mapper;
 import dto.EventDto;
 import model.Event;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
 public interface FromEventToEventDtoMapper extends BaseMapper<EventDto, Event> {
 
-    @Mapping(target = "file", ignore = true)
     @Override
     EventDto mappingFromEntityToDto(Event entity);
 
-    @Mapping(target = "file", ignore = true)
     @Override
     Event mappingFromDtoToEntity(EventDto dto);
 }
